13f2edf100725312ab64289ade1027fa57d86c5d
unknown <you@example.com>
2018-09-27 20:32:58 +0800

    city components

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,13 +1,78 @@
+
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    <li
+      class="item"
+      v-for="(item) of letters"
+      :key="item"
+      @click="hanldLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      :ref="item"
+    >
+      {{item}}
+    </li>
   </ul>
 </template>
 <script>
+/* 性能优化：
+* 1，const startY = this.$refs['A'][0].offsetTop 这个是固定值，每次滚动都会执行一次，在data中声明一个值，在生命钩子updated中，获取offsetTop的值
+*   原因：因为这个组件的list是从City中传过来的值，此时开始空数组，当值获取到之后，重新渲染了页面， updated会执行
+*
+* 2，函数节流：当在滚动的时候，会不断触发事件，使用setTimeout限制在一定时间内事件无法执行
+**/
 export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  /*性能优化1*/
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    hanldLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // const startY = this.$refs['A'][0].offsetTop // 获取A到顶部的距离
+          const touchY = e.touches[0].clientY// 获取移动点距离顶部的高度
+          const index = Math.floor((touchY - this.startY) / 20)// 获取每个标签的index
+          if (index >= 1 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+            console.log(this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
