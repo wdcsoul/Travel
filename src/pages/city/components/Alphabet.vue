523eecd088cc60fc66e69d883660e9876a3f70c8
unknown <you@example.com>
2018-09-27 17:42:46 +0800

    add city list css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" scoped>
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 0
+    right 0
+    bottom 0
+    width .4rem
+    background $bgColor
+    .item
+      line-height .4rem
+      text-align center
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="buttonList">
+          <div class="button-wrapper">
+           <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="buttonList">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+          <div class="item border-bottom">
+            aaa
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'CityList',
+  mounted () {
+    //  this.$refs.wrapper 获取dom zhuyi
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list /*让页面不会滚动*/
+    overflow hidden
+    position absolute
+    top:1.58rem
+    left 0
+    right 0
+    bottom 0
+  .title
+    line-height .54rem
+    background #eee
+    padding-left .2rem
+    font-size .26rem
+    color #666
+  .buttonList
+    padding .1rem .6rem .1rem .1rem
+    overflow hidden
+    .button-wrapper
+      float left
+      width 33.33%
+      .button
+        padding .1rem 0
+        text-align center
+        margin .1rem
+        border .02rem solid #ccc
+        border-radius .06rem
+  .item-list
+    .item
+      line-height .76rem
+      color #666
+      padding-left .2rem
+</style>
